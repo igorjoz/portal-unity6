38bf99ada1d22a6b332e69db6a766df12ec904fc
Igor Józefowicz <igorjozefowicz@gmail.com>
2025-11-08 22:04:20 +0100

    add lesson 11 progress

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public Texture2D map;
+    public ColorToPrefab[] colorMappings;
+    public float offset = 5f;
+    
+    void GenerateTile(int x, int z)
+    {
+        Color pixelColor = map.GetPixel(x, z);
+
+        if (pixelColor.a == 0)
+        {
+            return;
+        }
+
+        foreach(ColorToPrefab colorMapping in colorMappings)
+        {
+            if (colorMapping.color.Equals(pixelColor))
+            {
+                Vector3 position = new Vector3(x, 0, z) * offset;
+                Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
+            }
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
