1ba9d7cd5ecbbf7870feb28da167f0d3631c3fbc
Igor Józefowicz <igorjozefowicz@gmail.com>
2025-11-15 01:27:57 +0100

    Add map assets and update scene objects
    Added new map folder and TestMap.png asset. Updated SampleScene.unity to replace player and ground objects with new GroundNeutral and GroundHigh clones, and added new Wall objects. Introduced EditorButton script for editor functionality.

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
@@ -5,7 +7,10 @@ public class LevelGenerator : MonoBehaviour
     public Texture2D map;
     public ColorToPrefab[] colorMappings;
     public float offset = 5f;
-    
+
+    public Material material1;
+    public Material material2;
+
     void GenerateTile(int x, int z)
     {
         Color pixelColor = map.GetPixel(x, z);
@@ -15,7 +20,7 @@ public class LevelGenerator : MonoBehaviour
             return;
         }
 
-        foreach(ColorToPrefab colorMapping in colorMappings)
+        foreach (ColorToPrefab colorMapping in colorMappings)
         {
             if (colorMapping.color.Equals(pixelColor))
             {
@@ -25,13 +30,34 @@ public class LevelGenerator : MonoBehaviour
         }
     }
 
-    void Start()
+    public void GenerateLabirynth()
     {
-        
+        for (int x = 0; x < map.width; x++)
+        {
+            for (int z = 0; z < map.height; z++)
+            {
+                GenerateTile(x, z);
+            }
+        }
+
+        ColorTheChildren();
     }
 
-    void Update()
+    public void ColorTheChildren()
     {
-        
+        foreach (Transform child in transform)
+        {
+            if (child.tag == "Wall")
+            {
+                if (Random.Range(1, 100) % 4 == 0)
+                {
+                    child.gameObject.GetComponent<Renderer>().material = material1;
+                }
+                else
+                {
+                    child.gameObject.GetComponent<Renderer>().material = material2;
+                }
+            }
+        }
     }
 }
